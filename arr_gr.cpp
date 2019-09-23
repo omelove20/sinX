817e4f054b4d2c2efa1ebf882a0eb999a0b322b2
วีรพงษ์  นันตา <43377372+omelove20@users.noreply.github.com>
2019-09-23 22:29:54 +0700

    credit

--- arr_gr.cpp
+++ arr_gr.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 #include <string.h>
 #define MAXSTD 50
-#define IDLEN 4
+#define IDLEN 10
 #define NAMELEN 20
 #define MAXSJ 5
 #define SUBLEN 10
@@ -23,14 +23,16 @@
 
 	/*-----------------------------------------------------------------------------*/
 
-	int getSubject(char subject[MAXSJ][SUBLEN]){
+	int getSubject(char subject[MAXSJ][SUBLEN],float creditsj[MAXSJ]){
 		int n;
 		
 		printf("Enter number of Subject : ");
 		scanf("%d",&n);
 		for(int i=0;i<n;i++){
-			printf("Enter Name Subject%d [calculus] = ",i);
+			printf("Enter Name Subject %d [calculus] = ",i+1);
 			scanf("%s",subject[i]);
+			printf("Enter credit of Subject : ");
+			scanf("%f",&creditsj[i]);
 			
 		}
 		return n;
@@ -50,25 +52,33 @@
 		
 	/*-----------------------------------------------------------------------------*/	
 	
-	char setGr(int score[MAXSTD][MAXSJ],char* grade[MAXSTD][MAXSJ],int nbStd,int nbSub){
+	char setGr(int score[MAXSTD][MAXSJ],char* grade[MAXSTD][MAXSJ],int nbStd,int nbSub,float grnum[MAXSTD][MAXSJ]){
 		for(int i=0;i<nbStd;i++){
 			for(int j=0;j<nbSub;j++){
 				if(score[i][j]>=80){
 					grade[i][j]="A";
+					grnum[i][j]=4;
 				}else if(score[i][j]>=75){
 					grade[i][j]="B+";
+					grnum[i][j]=3.5;
 				}else if(score[i][j]>=70){
 					grade[i][j]="B";
+					grnum[i][j]=3;
 				}else if(score[i][j]>=65){
 					grade[i][j]="C+";
+					grnum[i][j]=2.5;
 				}else if(score[i][j]>=60){
 					grade[i][j]="C";
+					grnum[i][j]=2;
 				}else if(score[i][j]>=55){
 					grade[i][j]="D+";
+					grnum[i][j]=1.5;
 				}else if(score[i][j]>=50){
 					grade[i][j]="D";
+					grnum[i][j]=1;
 				}else{
 					grade[i][j]="F";
+					grnum[i][j]=0;
 				}
 			}
 		}
@@ -77,7 +87,7 @@
 	/*-----------------------------------------------------------------------------*/	
 	
 	void header(char subject[MAXSJ][SUBLEN],int nbSub){
-		printf("%10s%10s","ID","Name");
+		printf("%20s%10s","ID","Name");
 		for (int i=0;i<nbSub;i++){
 			printf("%10s",subject[i]);
 		}
@@ -87,26 +97,90 @@
 	void showstd(int nbStd,char id[MAXSTD][IDLEN],char name[MAXSTD][NAMELEN],char subject[MAXSJ][SUBLEN],int nbSub,char* grade[MAXSTD][MAXSJ]){
 		header(subject,nbSub);
 		for (int i=0;i<nbStd;i++){
-			printf("%10s%10s",id[i],name[i]);
+			printf("%10d%10s%10s",i+1,id[i],name[i]);
 			for (int j=0;j<nbSub;j++){
 				printf("%10s",grade[i][j]);
 			}
 			printf("\n");
 			
-		}
+		}		
 	}
 	
-	/*-----------------------------------------------------------------------------*/	
+	/*-----------------------------------------------------------------------------*/
+	
+	int countgr(int score[MAXSTD][MAXSJ],int nbStd,int nbSub){
+		int count=0;
+		for(int i=0; i<nbStd ; i++){
+			for(int j=0 ;j<nbSub ; j++){
+				if(score[i][j]<50){
+					count=count+1;
+				}	
+			}
+		}
+		return count;	
+	}
+		
+	/*-----------------------------------------------------------------------------*/
+/*	void chakgr(int score[MAXSTD][MAXSJ],int nbStd,int nbSub,char id[MAXSTD][IDLEN]){
+		char chakid[MAXSTD][NAMELEN];
+		int count=0;
+		printf("Enter number ID student = ");
+		scanf("%s",&chakid[0]);
+		
+		
+		for(int i=0;i<nbStd;i++){
+			if(chakid[0]==id[i]){
+				for(int j=0 ; j<nbSub ;j++){
+					if(score[i][j]<50){
+						count=count+1;
+						printf("***%d***",count);
+					}
+				}
+				printf("******");
+				
+			}
+			printf("******");
+		}
+		printf("\nStudent ID =%5s have gread F = %d",chakid[0],count);
+	}	
+	*/
+	
+	/*-----------------------------------------------------------------------------*/
+	
+	float showcd(	float grnum[MAXSTD][MAXSJ],float creditsj[MAXSJ],int nbStd,int nbSub,float sumgpa[MAXSTD][MAXSJ]){
+	float sumgr=0,sumcd=0,gpa;
+		for(int i=0;i<nbStd;i++){
+			for(int j=0;j<nbSub;j++){
+				sumgpa[i][j]=grnum[i][j]*creditsj[j];
+				sumgr=sumgr+sumgpa[i][j];
+				sumcd=sumcd+creditsj[j];
+			}
+		}
+		
+		gpa=sumgr/sumcd;
+		printf("%.2f**%.2f",sumcd,sumgr);
+		return gpa;
+	}
 	
 int main(){
 	char id[MAXSTD][IDLEN],name[MAXSTD][NAMELEN];
 	char subject[MAXSJ][SUBLEN];
 	int score[MAXSTD][MAXSJ];
 	char* grade[MAXSTD][MAXSJ];
+	int count=0;
 	int nbStd,nbSub;
+	float grnum[MAXSTD][MAXSJ];
+	float creditsj[MAXSJ];
+	float sumgpa[MAXSTD][MAXSJ];
+	
+	
 	nbStd = getStudent(id,name);
-	nbSub = getSubject(subject);
+	nbSub = getSubject(subject,creditsj);
 	getScore(subject,score,nbStd,nbSub,name);
-	setGr(score,grade,nbStd,nbSub);
+	setGr(score,grade,nbStd,nbSub,grnum);
 	showstd(nbStd,id,name,subject,nbSub,grade);
+	count=countgr(score,nbStd,nbSub);
+	printf("sum student F = %d",countgr(score,nbStd,nbSub));
+//	chakgr(score,nbStd,nbSub,id);
+	printf("\n\nGPA : %.2f",showcd(grnum,creditsj,nbStd,nbSub,sumgpa));
 }
